4598b31cec12c0e0eefe836aecffe5d9ab56005f
Fadil Eledath <fadilfaizal@gmail.com>
2014-12-29 10:46:17 +0000

    Long term changes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-window.onload = function() {
-  var body = document.body.scrollHeight;
-  footer.style.top = body+"px";
-}
+$(window).ready(function() {
+  setInterval(function() {
+    var winheight = $(window).height();
+    var bodheight = $('main').height() + $('#main').height() +$('footer').height()  + 55;
+    if (bodheight < winheight) {
+      $('footer').css("position", "absolute");
+      $('footer').css("bottom", "0");
+      $('footer').css("left", "0");
+    } else {
+      $('footer').css("position", "static");
+    }
+  }, 200);
+});
