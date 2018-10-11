bd4cd3abe374be6fa1a2e1b0c22ce5d10fd9488c
Fadil Eledath <fadilfaizal@gmail.com>
2018-10-11 19:37:38 +0400

    Refresh of profile

---
+++ assets/js/post.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(".galleryb").click(function(){
+		$("#gallery-container").css("display","block");
+	});
+	$("#gallery-back").click(function(){
+		$("#gallery-container").css("display","none");
+	});
+	$("#gallery .pic").click(function(){
+		if(!$(this).hasClass("viewing") && !$(this).hasClass("closing")){
+			$("#gallery .pic").removeClass("viewing");
+			$(this).addClass("viewing");
+		}
+	});
+	$("#gallery .pic button").click(function(){
+		$(this).parent().addClass("closing");
+		$(this).parent().removeClass("viewing");
+		setTimeout(function(){
+			$("#gallery .pic").removeClass("closing");
+		},100);
+	});
+});
--- script-empty.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-  var body = document.body.scrollHeight - 128;
-  footer.style.top = body+"px";
-}
